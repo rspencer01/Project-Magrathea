ba84d1ab2b96cb2f1300137cb7a93bb06e691f5c
Robert Spencer <robert.spencer94@gmail.com>
2013-06-27 15:33:23 +0200

    Neatened and commented sky code

--- sky.cpp
+++ sky.cpp
@@ -3,15 +3,20 @@
 #include "magrathea.h"
 #include "graphics.h"
 
-void RenderSky(float x, float y)
+void RenderSky(float x, float z)
 {
+	// No textures please
 	glBindTexture(GL_TEXTURE_2D,0);
+	// We will make a cone of triangles
 	glBegin(GL_TRIANGLE_FAN);
+	// A dark blue for the high sky.  The light colour is defined in magrathea.h
 	glColor3f(0.08984f,0.39453f,0.80859f);
-	glVertex3f(x,400.f,y);
+	glVertex3f(x,400.f,z);
 	glColor3fv(skyColor);
-	for (int i = 0;i<=20;i++)
-		glVertex3f(x+1000*sin(i/20.f*3.1415*2),0.f,y+1000*cos(i/20.f*3.1415*2));
+	// The sin and cos describes a circe surrounding the given position.
+	for (int i = 0;i<=21;i++)
+		glVertex3f(x+1000*sin(i/20.f*3.1415*2),0.f,z+1000*cos(i/20.f*3.1415*2));
 	glEnd();
-	addTriangles(20);
+	// And just for the hell of it, tell me how many triangles this contributes to the total.
+	addTriangles(21);
 }
